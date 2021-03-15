f8c43e4ae068d7459005ea32b0253fffa5149e0d
Andron-debug <b.a.a.2002@yandex.ru>
2021-03-15 17:42:33 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp 01.03.2021/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace WindowsFormsApp_01._03._2021
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxEND = new System.Windows.Forms.ComboBox();
+            this.comboBoxMID = new System.Windows.Forms.ComboBox();
+            this.comboBoxUP = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.true2 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checking_responses = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBoxEND
+            // 
+            this.comboBoxEND.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBoxEND.FormattingEnabled = true;
+            this.comboBoxEND.Items.AddRange(new object[] {
+            "Белый",
+            "Красный",
+            "Синий"});
+            this.comboBoxEND.Location = new System.Drawing.Point(12, 391);
+            this.comboBoxEND.Name = "comboBoxEND";
+            this.comboBoxEND.Size = new System.Drawing.Size(253, 39);
+            this.comboBoxEND.TabIndex = 26;
+            // 
+            // comboBoxMID
+            // 
+            this.comboBoxMID.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBoxMID.FormattingEnabled = true;
+            this.comboBoxMID.Items.AddRange(new object[] {
+            "Белый",
+            "Красный",
+            "Синий"});
+            this.comboBoxMID.Location = new System.Drawing.Point(12, 346);
+            this.comboBoxMID.Name = "comboBoxMID";
+            this.comboBoxMID.Size = new System.Drawing.Size(253, 39);
+            this.comboBoxMID.TabIndex = 25;
+            // 
+            // comboBoxUP
+            // 
+            this.comboBoxUP.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBoxUP.FormattingEnabled = true;
+            this.comboBoxUP.Items.AddRange(new object[] {
+            "Белый",
+            "Красный",
+            "Синий"});
+            this.comboBoxUP.Location = new System.Drawing.Point(12, 301);
+            this.comboBoxUP.Name = "comboBoxUP";
+            this.comboBoxUP.Size = new System.Drawing.Size(253, 39);
+            this.comboBoxUP.TabIndex = 24;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(7, 262);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(464, 25);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "№3 Выберите проавлбный порядок цветов";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 14.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(10, 57);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(225, 50);
+            this.textBox1.TabIndex = 22;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Controls.Add(this.true2);
+            this.groupBox2.Location = new System.Drawing.Point(3, 144);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(115, 106);
+            this.groupBox2.TabIndex = 21;
+            this.groupBox2.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radioButton1.Location = new System.Drawing.Point(6, 17);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 36);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Да";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // true2
+            // 
+            this.true2.AutoSize = true;
+            this.true2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.true2.Location = new System.Drawing.Point(6, 58);
+            this.true2.Name = "true2";
+            this.true2.Size = new System.Drawing.Size(72, 35);
+            this.true2.TabIndex = 6;
+            this.true2.TabStop = true;
+            this.true2.Text = "Нет";
+            this.true2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(0, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(762, 25);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "№2 Существует ли официальная трактовка цветов российского флага?";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(0, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(603, 25);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "№1 В каком году был утвержден российский триколор?";
+            // 
+            // checking_responses
+            // 
+            this.checking_responses.BackColor = System.Drawing.Color.Purple;
+            this.checking_responses.ForeColor = System.Drawing.Color.White;
+            this.checking_responses.Location = new System.Drawing.Point(642, 216);
+            this.checking_responses.Name = "checking_responses";
+            this.checking_responses.Size = new System.Drawing.Size(159, 121);
+            this.checking_responses.TabIndex = 18;
+            this.checking_responses.Text = "Проверить";
+            this.checking_responses.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBoxEND);
+            this.Controls.Add(this.comboBoxMID);
+            this.Controls.Add(this.comboBoxUP);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checking_responses);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxEND;
+        private System.Windows.Forms.ComboBox comboBoxMID;
+        private System.Windows.Forms.ComboBox comboBoxUP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton true2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button checking_responses;
+    }
+}
---
+++ WindowsFormsApp 01.03.2021/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_01._03._2021
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rez = 0;
+            if (textBox1.Text == "1991")
+            {
+                rez++;
+                label1.ForeColor = System.Drawing.Color.Green;
+
+            }
+            else
+            {
+                label1.ForeColor = System.Drawing.Color.Red;
+                textBox1.Text = "1991";
+                textBox1.ForeColor = System.Drawing.Color.Green;
+            }
+            if (true2.Checked)
+            {
+                rez++;
+                label2.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                label2.ForeColor = System.Drawing.Color.Red;
+                true2.ForeColor = System.Drawing.Color.Green;
+            }
+            //0.Белый
+            //1.Красный
+            //2.Синий
+            if ((comboBoxUP.SelectedItem == "Белый") & (comboBoxMID.SelectedItem == "Синий") & (comboBoxEND.SelectedItem == "Красный"))
+            {
+                rez++;
+                label3.ForeColor = System.Drawing.Color.Green;
+                comboBoxUP.ForeColor = System.Drawing.Color.Gray;
+                comboBoxMID.ForeColor = System.Drawing.Color.Blue;
+                comboBoxEND.ForeColor = System.Drawing.Color.Red;
+
+            }
+            else
+            {
+                label3.ForeColor = System.Drawing.Color.Red;
+                comboBoxUP.SelectedItem = "Белый";
+                comboBoxMID.SelectedItem = "Синий";
+                comboBoxEND.SelectedItem = "Красный";
+
+                comboBoxUP.ForeColor = System.Drawing.Color.Gray;
+                comboBoxMID.ForeColor = System.Drawing.Color.Blue;
+                comboBoxEND.ForeColor = System.Drawing.Color.Red;
+            }
+            MessageBox.Show(rez + " из 3");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
